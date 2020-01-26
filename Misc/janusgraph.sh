bf17351750bd6c397a45cfd78d11bfde9bfe08fa
Md. Farhan Hasin <opangkteyo@gmail.com>
2020-01-26 19:05:11 +0600

    update janusgraph docker command

--- Misc/janusgraph.sh
+++ Misc/janusgraph.sh
@@ -1,6 +1,5 @@
 JanusGraph Setup: https://docs.janusgraph.org/getting-started/installation/
 
     1. Get docker from here: https://docs.docker.com/install/linux/docker-ce/ubuntu/
-    2. Start Server: Start a JanusGraph Server instance from Docker Image : sudo docker run --name janusgraph-default janusgraph/janusgraph:latest
+    2. Start Server: Start a JanusGraph Server instance from Docker Image : sudo docker run -p 8182:8182 --name janusgraph-default janusgraph/janusgraph:latest
     3. Start Gremlin Client: Get into bash shell of 'janusgraph-default' and run bin/gremlin.sh
-    4. 
